54f78546611b744d5184a04466992ad129b6e59f
killinq-joke <zakadmaster@gmail.com>
2020-05-02 04:31:28 +0200

    Addition of state folder

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import '../style/App.css';
+//dependencies
+import React from "react";
+import { Link, Redirect, Route} from "react-router-dom";
+//styles
+import "./styles/App.css";
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+  return <div className="App">
+    
+  </div>;
 }
 
 export default App;

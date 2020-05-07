1d6d40cd77bbab316758114358d8fd309f7fc90c
killinq-joke <zakadmaster@gmail.com>
2020-05-07 23:32:46 +0200

    Addition of project funding info

--- client/src/components/Project.js
+++ client/src/components/Project.js
@@ -1,12 +1,14 @@
 import React from "react"
 
 const Project = ({project}) => {
+  console.log(project)
     return (
       <>
         <h4>{project.name}</h4>
         <p>{project.description}</p>
         <p>category: {project.category}</p>
         <img alt="" src={project.img_url}/>
+        <p>funding: {project.funding} / {project.funding_goal}</p>
       </>
     );
 }

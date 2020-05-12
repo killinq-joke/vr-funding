b0e7bb3fb6089bfa412ea0584aec4ada0b1dfe47
killinq-joke <zakadmaster@gmail.com>
2020-05-12 05:32:02 +0200

    Addition of ProjectById rendering

--- client/src/components/ProjectById.js
+++ client/src/components/ProjectById.js
@@ -1,16 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import axios from "../utils/axiosWithAuth";
 import { useParams } from "react-router-dom";
 
+import Project from "./Project";
+
 export default function ProjectById() {
   const { project_id } = useParams();
-  axios()
-    .get(`/projects/${project_id}`)
-    .then((res) => {
-      console.log(res);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-  return <>funded projects</>;
+  const [project, setProject] = useState({});
+  useEffect(() => {
+    axios()
+      .get(`/projects/${project_id}`)
+      .then((res) => {
+        setProject(res.data);
+        console.log(res.data.name);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  return (
+    <>
+      <Project key={project_id} project={project} />
+    </>
+  );
 }

37590908374dee5d41a52f6e4d2804b545225c5e
killinq-joke <zakadmaster@gmail.com>
2020-05-12 02:35:07 +0200

    Changed error handling YourProjects.js

--- client/src/components/Project.js
+++ client/src/components/Project.js
@@ -1,18 +1,20 @@
-import React from "react"
+import React from "react";
 
-const Project = ({project}) => {
-  const user_id = Number(localStorage.getItem("user_id"))
-  
-    return (
-      <>
-        <h4>{project.name}</h4>
-        <p>{project.description}</p>
-        <p>category: {project.category}</p>
-        <img alt="" src={project.img_url}/>
-        <p>funding: {project.funding} / {project.funding_goal}</p>
-        {user_id === project.creator_id ? null : <button>fund</button>}
-      </>
-    );
-}
+const Project = ({ project }) => {
+  const user_id = Number(localStorage.getItem("user_id"));
 
-export default Project
+  return (
+    <>
+      <h4>{project.name}</h4>
+      <p>{project.description}</p>
+      <p>category: {project.category}</p>
+      <img alt="" src={project.img_url} />
+      <p>
+        funding: {project.funding} / {project.funding_goal}
+      </p>
+      {user_id === project.creator_id ? null : <button>fund</button>}
+    </>
+  );
+};
+
+export default Project;

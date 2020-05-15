137d776da3204cd3ed3e2f33b1466da9a3b0e53e
killinq-joke <zakadmaster@gmail.com>
2020-05-15 04:58:56 +0200

    Formatting

--- client/src/components/ProjectList.js
+++ client/src/components/ProjectList.js
@@ -1,24 +1,25 @@
 import React, { useEffect, useState } from "react";
 import axios from "../utils/axiosWithAuth";
 //components
-import Project from "./Project"
+import Project from "./Project";
 
 export default function ProjectList() {
-    const [projects, setProjects] = useState([])
-    useEffect(() => {
-        axios()
-        .get("/projects")
-        .then(async res => {
-            await setProjects(res.data.projects);
-        }).catch(err => {
-            console.log(err)
-        })
-    }, [])
+  const [projects, setProjects] = useState([]);
+  useEffect(() => {
+    axios()
+      .get("/projects")
+      .then(async (res) => {
+        await setProjects(res.data.projects);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
   return (
     <>
       project list
-      {projects.map(project => {
-          return <Project key={project.id} project={project}/>
+      {projects.map((project) => {
+        return <Project key={project.id} project={project} />;
       })}
     </>
   );

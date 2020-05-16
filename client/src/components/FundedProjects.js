70044146b3f7319c455e6f217bd7ed58b267c825
killinq-joke <zakadmaster@gmail.com>
2020-05-16 02:39:22 +0200

    Addition of project state for fundedprojects

--- client/src/components/FundedProjects.js
+++ client/src/components/FundedProjects.js
@@ -3,13 +3,14 @@ import axios from "../utils/axiosWithAuth";
 
 export default function FundedProjects() {
   const [error, setError] = useState("")
+  const [projects, setProjects] = useState([]);
   const funder_id = localStorage.getItem("user_id")
 
   useEffect(() => {
     axios() 
       .get(`/funders/projects/${funder_id}`)
       .then((res) => {
-        console.log(res);
+        console.log(res.data);
       })
       .catch((err) => {
         setError(err.response.data.message);

e8867d375aa3bb9c6801f36bb5fe8db74fb47be1
killinq-joke <zakadmaster@gmail.com>
2020-05-16 02:35:25 +0200

    Addition of ajax request for funder projects

--- client/src/components/FundedProjects.js
+++ client/src/components/FundedProjects.js
@@ -1,5 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
+import axios from "../utils/axiosWithAuth";
 
 export default function FundedProjects() {
+  
+  const funder_id = localStorage.getItem("user_id")
+
+  useEffect(() => {
+    axios() 
+      .get(`/funders/projects/${funder_id}`)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err.response.data.message);
+      });
+  }, []);
   return <>funded projects</>;
 }

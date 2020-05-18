ccf065f148fb5a43c6c756909bad0565b481adf6
killinq-joke <zakadmaster@gmail.com>
2020-05-18 17:57:56 +0200

    CategoryNav in use

--- client/src/components/CategoryNav.js
+++ client/src/components/CategoryNav.js
@@ -1,5 +1,26 @@
-import React from "react"
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function CategoryNav() {
-    return <></>
+  return (
+    <>
+      <nav>
+        <Link to="/art">
+          <button>Art</button>
+        </Link>
+        <Link to="/cinema">
+          <button>Cinema</button>
+        </Link>
+        <Link to="/health">
+          <button>Health</button>
+        </Link>
+        <Link to="/gaming">
+          <button>Gaming</button>
+        </Link>
+        <Link to="/music">
+          <button>Music</button>
+        </Link>
+      </nav>
+    </>
+  );
 }
